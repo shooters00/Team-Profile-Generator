f9dea9fcc49a86b0f64b666ea746e9bfd89b64cc
shooters00 <shooters0000@gmail.com>
2021-08-01 21:47:29 -0400

    Questions working

--- index.js
+++ index.js
@@ -6,39 +6,145 @@ const Employee = require('./lib/Employee');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
+const makeHTML = require('./lib/makeHTML');
+const employeeQuestions = require('./lib/empQuest');
+let managerQuestions = require('./lib/manQuest');
+let internQuestions = require('./lib/intQuest');
+let engineerQuestions = require('./lib/engQuest');
+managerQuestions = employeeQuestions.concat(managerQuestions);
+internQuestions = employeeQuestions.concat(internQuestions);
+engineerQuestions = employeeQuestions.concat(engineerQuestions);
+let teamArray = [];
 
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'What is the team manager\'s name?',
-        },
-        {
-            type: 'input',
-            name: 'id',
-            message: 'What is the team manager\'s employee id?',
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'What is the team manager\'s email address?',
-        },
-        {
-            type: 'input',
-            name: 'officeNumber',
-            message: 'What is the team manager\'s officeNumber?',
-        },
-    ])
+const start = () => {
+    inquirer
+    .prompt(managerQuestions)
     .then((data) => {
-        console.log(data);
-    });
+        data.role = 'Manager';
+        //teamArray.push(data);
+        //console.log(teamArray);
+        addToTeam(data);
+        moreEmployees();
+    })
+    .catch((err) => console.log(err));
+}
+
+const moreEmployees = () => {
+    inquirer
+    .prompt(
+        [
+            {
+                type: 'list',
+                name: 'continue',
+                message: 'Do you want to add more team members?',
+                choices: ['Yes', 'No']
+            }
+        ]
+    )
+    .then((input) => {
+        if (input.continue === 'Yes') {
+            addMore();
+        } else {
+            module.exports = teamArray;
+            makeHTML(teamArray);
+        }
+    })
+    .catch((err) => console.log(err));
+}
+
+const addMore = () => {
+    inquirer
+    .prompt(
+        [
+            {
+                type: 'list',
+                name: 'whichOne',
+                message: 'Do you want to add an Engineer or an Intern?',
+                choices: ['Engineer', 'Intern']
+            }
+        ]
+    )
+    .then((choice) => {
+        if (choice.whichOne === 'Engineer') {
+            addEngineer();
+        } else {
+            addIntern();
+        }
+    })
+    .catch((err) => console.log(err));
+}
+
+const addEngineer = () => {
+    console.log('Made it to add Engineer function');
+    inquirer
+    .prompt(engineerQuestions)
+    .then((data) => {
+        data.role = 'Engineer';
+        //teamArray.push(data);
+        //console.log(teamArray);
+        addToTeam(data);
+        moreEmployees();
+    })
+    .catch((err) => console.log(err));
+}
+
+const addIntern = () => {
+    console.log('Made it to add Intern function');
+    inquirer
+    .prompt(internQuestions)
+    .then((data) => {
+        data.role='Intern';
+        //teamArray.push(data);
+        //console.log(teamArray);
+        addToTeam(data);
+        moreEmployees();
+    })
+    .catch((err) => console.log(err));
+}
+
+const addToTeam = ({ role, name, id, email, officeNumber, github, school}) => {
+    let teamMember;
+    //console.log(`role: ${role}`);
+    switch(role) {
+        case 'Manager':
+            teamMember = new Manager(name, id, email, officeNumber);
+            break;
+        case 'Intern':
+            teamMember = new Intern(name, id, email, school);
+            break;
+        case 'Engineer':
+            teamMember = new Engineer(name, id, email, github);
+            break;
+        default:
+            //module.exports = teamArray;
+            //makeHTML(teamArray);
+            //return;
+    }
+    console.log('Team Member:');
+    console.log(teamMember);
+    teamArray.push(teamMember);
+    console.log('Team Array');
+    console.log(teamArray);
+}
+
+/*
+const makeHTML = () => {
+    console.log('Made it to make HTML function');
+    
+}
+*/
+
+console.log("Starting...");
+start();
 
 
 
-    //<i class="fas fa-glasses"></i>
-    //<i class="fas fa-mug-hot"></i>
-    //<i class="fas fa-user-graduate"></i>
-    //could assign a class per role
-    //create a separate js to build the html for the roles
-    //
+    /*
+    const filename = "README.md";
+    fs.writeFile(filename, index, (err) => {
+      err ? console.log(err) : console.log('Success!')
+      open('README.md', { "wait": true });
+    }
+    );
+    */
+
